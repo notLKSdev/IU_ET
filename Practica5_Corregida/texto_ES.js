bf01323d6f0264d12635b4c70672fa9243175502
Lookos <lookoszero@gmail.com>
2026-05-23 19:11:39 +0200

    Quitar carpetas extra

--- Practica5_Corregida/texto_ES.js
+++
@@ -1,8 +0,0 @@
-let texto_ES={
-    //general pagina
-    "titulo_pagina_personas":"Gestion de personas",
-    "KO_nombre_personas_tam_min": "nombre minimo peta",
-    "KO_nombre_personas_tam_max": "nombre maximo peta",
-};
-
-let traduccion = texto_ES;
